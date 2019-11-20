9040caee9d0eec4aa15e6fdf236c98f41b6cefb8
Evgen <eok8177@gmail.com>
2019-11-20 09:56:39 +0200

    backend dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\TelegramUser;
+use App\Client;
+
 class DashboardController extends Controller
 {
     public function index() {
-        return view('backend.index');
+        return view('backend.index', [
+            'clients' => Client::all(),
+            'tUsers' => TelegramUser::all(),
+        ]);
     }
 }
--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -2,6 +2,55 @@
 
 @section('content')
 <div class="container">
-    <h1>Backend Dashboard</h1>
+    <h5>Clients</h5>
+
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Telegram ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Country</th>
+          <th scope="col">City</th>
+          <th scope="col">Address</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($clients as $client)
+        <tr>
+          <th scope="row">{{$client->t_id}}</th>
+          <td>{{$client->last_name}} {{$client->first_name}} {{$client->sur_name}}</td>
+          <td>{{$client->country}}</td>
+          <td>{{$client->city}}</td>
+          <td>{{$client->address}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+
+    <h5>Telegram Users</h5>
+
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Telegram ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Username</th>
+          <th scope="col">Current step</th>
+          <th scope="col">Last message</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($tUsers as $user)
+        <tr>
+          <th scope="row">{{$user->id}}</th>
+          <td>{{$user->last_name}} {{$user->first_name}}</td>
+          <td>{{$user->username}}</td>
+          <td>{{$user->section}} {{$user->step}}</td>
+          <td>{{$user->text}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
 </div>
 @endsection
